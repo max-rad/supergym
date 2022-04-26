1c6865700de817f053ff506e7c84909800291bb6
max_rad <maxim_radionov@bk.ru>
2022-04-27 01:53:23 +0300

    Вносит правки

--- source/js/modules/carousel/carousel.js
+++ source/js/modules/carousel/carousel.js
@@ -1,68 +1,25 @@
+import Swiper, {Navigation} from 'swiper';
+
 const carousel = () => {
+  Swiper.use([Navigation]);
+
   const carouselWrapper = document.querySelector('[data-carousel="carousel"]');
-  const carouselItems = carouselWrapper.querySelectorAll('[data-carousel="slide"]');
 
   const carouselLeftButton = carouselWrapper.querySelector('[data-carousel="left-button"]');
   const carouselRightButton = carouselWrapper.querySelector('[data-carousel="right-button"]');
 
   carouselWrapper.classList.remove('reviews__content-wrapper--no-js');
 
-  let index = 0;
-  let animateFrames;
+  const swiper = new Swiper('.swiper2', {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 40,
 
-  const ANIMATE_OPTIONS = {
-    duration: 400,
-    iterations: 1,
-  };
-
-  carouselLeftButton.addEventListener('click', onClickLeftButton);
-
-  carouselRightButton.addEventListener('click', onClickRightButton);
-
-  function showSlides() {
-    for (let i = 0; i < carouselItems.length; i++) {
-      carouselItems[i].classList.remove('reviews__item--active');
-    }
-
-    carouselItems[index].classList.add('reviews__item--active');
-    carouselItems[index].animate(animateFrames, ANIMATE_OPTIONS);
-  }
-
-  function onClickLeftButton() {
-    index--;
-
-    animateFrames = [
-      {transform: 'translateX(0px)', opacity: '0'},
-      {transform: 'translateX(-5px)', opacity: '0.25'},
-      {transform: 'translateX(-10px)', opacity: '0.5'},
-      {transform: 'translateX(-15px)', opacity: '0.75'},
-      {transform: 'translateX(-20px)', opacity: '1'}
-    ];
-
-    if (index < 0) {
-      index = carouselItems.length - 1;
-    }
-
-    showSlides();
-  }
-
-  function onClickRightButton() {
-    index++;
-
-    animateFrames = [
-      {transform: 'translateX(0px)', opacity: '0'},
-      {transform: 'translateX(5px)', opacity: '0.25'},
-      {transform: 'translateX(10px)', opacity: '0.5'},
-      {transform: 'translateX(15px)', opacity: '0.75'},
-      {transform: 'translateX(20px)', opacity: '1'}
-    ];
-
-    if (index >= carouselItems.length) {
-      index = 0;
-    }
-
-    showSlides();
-  }
+    navigation: {
+      prevEl: carouselLeftButton,
+      nextEl: carouselRightButton,
+    },
+  });
 };
 
 export {carousel};
